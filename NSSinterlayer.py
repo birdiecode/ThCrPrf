eb47aeb8a6680711d889a0a4251ebdbbd54786de
birdiecode <birdiecode@gmail.com>
2023-11-10 09:51:51 +0300

    добалено инициализация nss

--- NSSinterlayer.py
+++ NSSinterlayer.py
@@ -1,10 +1,27 @@
+import ctypes
+
+
 class NSSinterlayer:
 
     def __init__(self):
-        pass
+        nsslib = "/usr/lib/x86_64-linux-gnu/libnss3.so"
+        self.libnss = ctypes.CDLL(nsslib)
+
+        self.NSS_Init = self.libnss.NSS_Init
+        self.NSS_Init.argtypes = [ctypes.c_char_p]
+        self.NSS_Init.restype = ctypes.c_int
+
+        self.NSS_Shutdown = self.libnss.NSS_Shutdown
+        self.NSS_Init.restype = ctypes.c_int
+
+    def init(self, path):
+        return self.NSS_Init(str("sql:"+path).encode("utf-8"))
 
     def encrypt(self, plaintext):
         pass
 
     def dectypt(self, endata):
         pass
+
+    def close(self):
+        return self.NSS_Shutdown()
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from NSSinterlayer import NSSinterlayer
 
 nss = NSSinterlayer()
+print(nss.init("./key"))
+print(nss.close())
+
 data = nss.encrypt("test_text")
 print(data)
 print(nss.dectypt(data))
