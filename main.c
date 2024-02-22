caa376d8c9420de93f0b1d288f918b71ccecebcb
birdiecode <birdiecode@gmail.com>
2024-02-22 12:50:16 +0300

    переписал на си так как так удобнее

---
+++ main.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <windows.h>
+
+typedef enum {
+    SECWouldBlock = -2,
+    SECFailure = -1,
+    SECSuccess = 0
+} SECStatus;
+
+typedef void* PK11SlotInfo;          /* defined in secmodti.h */
+
+typedef enum {
+    PR_FALSE = 0,
+    PR_TRUE = 1
+} PRBool;
+
+typedef enum {
+    siBuffer = 0,
+    siClearDataBuffer = 1,
+    siCipherDataBuffer = 2,
+    siDERCertBuffer = 3,
+    siEncodedCertBuffer = 4,
+    siDERNameBuffer = 5,
+    siEncodedNameBuffer = 6,
+    siAsciiNameString = 7,
+    siAsciiString = 8,
+    siDEROID = 9,
+    siUnsignedInteger = 10,
+    siUTCTime = 11,
+    siGeneralizedTime = 12,
+    siVisibleString = 13,
+    siUTF8String = 14,
+    siBMPString = 15
+} SECItemType;
+
+typedef struct SECItemStr SECItem;
+
+struct SECItemStr {
+    SECItemType type;
+    unsigned char *data;
+    unsigned int len;
+};
+
+typedef struct {
+    enum {
+        PW_NONE = 0,
+        PW_FROMFILE = 1,
+        PW_PLAINTEXT = 2,
+        PW_EXTERNAL = 3
+    } source;
+    char *data;
+} secuPWData;
+
+typedef SECStatus (*func1)(char *profile); // NSS_InitReadWrite
+typedef PK11SlotInfo (*func2)(); // PK11_GetInternalKeySlot
+typedef void (*func3)(PK11SlotInfo *slot); // PK11_FreeSlot
+typedef PRBool (*func4)(PK11SlotInfo *slot); // PK11_NeedUserInit
+typedef SECStatus (*func5)(PK11SlotInfo *slot, const char *ssopw,
+        const char *pk11_userpwd); // PK11_InitPin
+typedef SECStatus (*func6)(SECItem *keyid, SECItem *data,
+        SECItem *result, void *cx); // PK11SDR_Encrypt
+typedef char* (*func7)(SECItem *keyid, SECItem *data, int type,
+        SECItem *input); // NSSBase64_EncodeItem
+
+int main() {
+    SECStatus rv;
+    HINSTANCE hDll = LoadLibrary("nss3.dll");
+
+    func1 NSS_InitReadWrite;
+    func2 PK11_GetInternalKeySlot;
+    func3 PK11_FreeSlot;
+    func4 PK11_NeedUserInit;
+    func5 PK11_InitPin;
+    func6 PK11SDR_Encrypt;
+    func7 NSSBase64_EncodeItem;
+
+
+    if (hDll != NULL) {
+        printf("Load library ok\n");
+
+        NSS_InitReadWrite = (func1)GetProcAddress(hDll, "NSS_InitReadWrite");
+        if (NSS_InitReadWrite == NULL) return 2;
+
+        PK11_GetInternalKeySlot = (func2)GetProcAddress(hDll, "PK11_GetInternalKeySlot");
+        if (PK11_GetInternalKeySlot == NULL) return 3;
+
+        PK11_FreeSlot = (func3)GetProcAddress(hDll, "PK11_FreeSlot");
+        if (PK11_FreeSlot == NULL) return 4;
+
+        PK11_NeedUserInit = (func4)GetProcAddress(hDll, "PK11_NeedUserInit");
+        if (PK11_NeedUserInit == NULL) return 5;
+
+        PK11_InitPin = (func5)GetProcAddress(hDll, "PK11_InitPin");
+        if (PK11_InitPin == NULL) return 6;
+
+        PK11SDR_Encrypt = (func6)GetProcAddress(hDll, "PK11SDR_Encrypt");
+        if (PK11SDR_Encrypt == NULL) return 7;
+
+        NSSBase64_EncodeItem = (func7)GetProcAddress(hDll, "NSSBase64_EncodeItem");
+        if (NSSBase64_EncodeItem == NULL)
+            NSSBase64_EncodeItem = (func7)GetProcAddress(hDll, "NSSBase64_EncodeItem_Util");
+        if (NSSBase64_EncodeItem == NULL) return 8;
+
+    } else {
+        DWORD error = GetLastError();
+        printf("Error load library code: %u\n", error);
+        return 1;
+    }
+
+    rv = NSS_InitReadWrite("sql:C:\\w\\test");
+    if (rv != SECSuccess) {
+        return 20;
+    }
+    PK11SlotInfo *slot = NULL;
+    slot = PK11_GetInternalKeySlot();
+
+    if (slot && PK11_NeedUserInit(slot)) {
+        printf("PK11_NeedUserInit and KeySlot ok\n");
+        rv = PK11_InitPin(slot, NULL, "");
+        if (rv != SECSuccess) {
+            return 21;
+        }
+        printf("Set empty password\n");
+    }
+    if (slot) {
+        PK11_FreeSlot(slot);
+    }
+
+    char *teststr = "hui";
+
+    SECItem keyid = { 0, 0, 0 };
+    SECItem data;
+    data.data = (unsigned char *)teststr;
+    data.len = strlen(teststr);
+
+    SECItem result = { 0, 0, 0 };
+    secuPWData pwdata = { PW_NONE, 0 };
+
+
+    rv = PK11SDR_Encrypt(&keyid, &data, &result, &pwdata);
+    if (rv != SECSuccess) {
+        return 22;
+    }
+    printf("Encrypt text ok\n");
+
+    //dd = self._NSSBase64_EncodeItem_Util(None, None, 0, out)
+    char *newResult = NSSBase64_EncodeItem(NULL, NULL, 0, &result);
+    printf("%s\n", newResult);
+
+    //char *profile_path = "sql:C:\\Users\\admin\\Desktop\\profileth";
+    FreeLibrary(hDll);
+    return 0;
+}
+
